72e9ff4acb9c0f42a144ad4f3ba77ccc000c3d5b
briansteuber <brian.steuber18@gmail.com>
2021-06-05 14:48:02 -0700

    updated config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-west-2_4cmY45btl', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4gp401940g1ho1db7vh8ik3aqi', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
